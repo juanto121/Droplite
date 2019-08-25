9ef9ea75b5a2bb1a91ff490b1f88ab6108ff3647
Juanto121 <juanf.torres.lopez@gmail.com>
2019-08-25 13:49:56 -0500

    Storybook setup #2

---
+++ stories/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Draglist } from "../src/components";
+
+storiesOf('Simple DragList', module)
+  .add('Without styling', () => (
+    <div>Test storybook</div>
+  ))
